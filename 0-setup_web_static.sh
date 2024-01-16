bca7b6460cd55ca85138a6fde13ae7930d2c3142
whitney507 <whitneymarian507@gmail.com>
2024-01-16 05:49:54 -0800

    Refactored 0-setup_web_static.sh to adhere to line length conventions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,6 +23,7 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias
+/data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
